b7d36b8b6ebeeddb548984be952ce924330a5be0
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2020-06-16 08:02:36 -0400

    Issue #34 - Continue working HTML controls for clearing data

--- Poke-Farm-QoL.user.js
+++ Poke-Farm-QoL.user.js
@@ -174,6 +174,14 @@
                         }
                     }
                 },
+                clearPageSettings(pageName) {
+                    console.log('fn.backwork.clearPageSettings - ', pageName)
+                    if(! (pageName in PAGES) ) {
+                        console.error(`Could not proceed with clearing page settings. Page ${pageName} not found in list of pages`)
+                    } else {
+                        PAGES[pageName].resetSettings();
+                    }
+                },
                 setupHTML() { // injects the HTML changes from TEMPLATES into the site
                     // Header link to Userscript settings
                     document.querySelector("li[data-name*='Lucky Egg']").insertAdjacentHTML('afterend', TEMPLATES.qolHubLinkHTML);
@@ -229,7 +237,7 @@
                         'loading Settings'    : fn.backwork.loadSettings,
                         'checking for update' : fn.backwork.checkForUpdate,
                         'setting up HTML'     : fn.backwork.setupHTML,
-			'populating Settings' : fn.backwork.populateSettingsPage,
+                        'populating Settings' : fn.backwork.populateSettingsPage,
                         'setting up CSS'      : fn.backwork.setupCSS,
                         'setting up Observers': fn.backwork.setupObservers,
                         'setting up Handlers' : fn.backwork.setupHandlers,
@@ -307,7 +315,15 @@
                             }
                         }
                     }
+                },
+
+                clearPageSettings(pageName) {
+                    console.log(pageName)
+                    if(pageName !== "None") { // "None" matches option in HTML
+                        fn.backwork.clearPageSettings(pageName)
+                    }
                 }
+
             }, // end of API
         }; // end of fn
 
@@ -410,6 +426,16 @@
                 progressSpan.textContent = "Complete!"
             }
         }) // loadDexPage
+    }));
+
+    $(document).on('click', '#resetPageSettings', (function() {
+        const page = $(this).parent().find('select').val()
+        console.log("Clicked resetPageSettings, page = ", page)
+        PFQoL.clearPageSettings(page)
+    }));
+
+    $(document).on('click', '#clearCachedDex', (function() {
+        console.log("Clicked clearCachedDex")
     }));
 
     $(document).on('click', 'h3.slidermenu', (function() { //show hidden li in change log
