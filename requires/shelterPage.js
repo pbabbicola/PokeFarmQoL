04f139215d967d863aec482d2b5a35d96e39ed4c
Jon Paul Gualdarrama <gualdarrama.jon@gmail.com>
2020-07-14 22:43:52 -0400

    Issue #11 - Add highlighting to shelter page

--- requires/shelterPage.js
+++ requires/shelterPage.js
@@ -19,6 +19,7 @@ class ShelterPage extends Page {
             findMale: true,
             findFemale: true,
             findNoGender: true,
+            findNFE: false,
             customEgg: true,
             customPokemon: true,
             customPng: false,
@@ -219,6 +220,50 @@ class ShelterPage extends Page {
 
     }
 
+    highlightByHowFullyEvolved(pokemon_elem) {
+        // if a pokemon is clicked-and-dragged, the tooltip element after the pokemon
+        // will not exist. If this occurs. don't try highlighting anything until the
+        // pokemon is "put down"
+        if(!$(pokemon_elem).next().length) { return; }
+
+        const tooltip_elem = $(pokemon_elem).next()[0];
+        const tooltip = {
+            species: tooltip_elem.textContent.split(' '),
+            forme: ''
+        }
+        let pokemon = tooltip['species'];
+
+        const key = 'QoLEvolutionTreeDepth'
+        if(localStorage.getItem(key) !== null) {
+            const evolution_data = JSON.parse(localStorage.getItem(key))
+            if(Object.keys(evolution_data).length > 0) {
+                // if can't find the pokemon directly, try looking for its form data
+                if(!evolution_data[pokemon]) {
+                    if(tooltip['forme']) {
+                        pokemon = pokemon + ' [' + tooltip['forme'] + ']'
+                    }
+                }
+                if(!evolution_data[pokemon]) {
+                    console.error(`Private Fields Page - Could not find evolution data for ${pokemon}`);
+                } else {
+                    const evolutions_left = evolution_data[pokemon].remaining
+                    const evolution_tree_depth = evolution_data[pokemon].total
+
+                    if(evolutions_left === 1) {
+                        $(pokemon_elem).children('img.big').addClass('oneevolutionleft');
+                    } else if(evolutions_left === 2) {
+                        $(pokemon_elem).children('img.big').addClass('twoevolutionleft');
+                    }
+
+                }
+            } else {
+                console.error('Unable to load evolution data. In QoL Hub, please clear cached dex and reload dex data');
+            }
+        } else {
+            console.error('Unable to load evolution data. In QoL Hub, please clear cached dex and reload dex data');
+        }
+    }
+
     customSearch() {
         const obj = this;
         const SEARCH_DATA = GLOBALS.SHELTER_SEARCH_DATA;
@@ -273,6 +318,11 @@ class ShelterPage extends Page {
         }
         if(this.settings.findCustomSprite === true) {
             this.searchForImgTitle('findCustomSprite')
+        }
+        if(this.settings.findNFE === true) {
+            $('#shelterarea>.pokemon').each(function() {
+                obj.highlightByHowFullyEvolved(this)
+            })
         }
 
         if(this.settings.findNewPokemon === true) {
